10ad02a47efc943693f159ec274284a6c4baf3bd
SaifAllahGharib <saifallahgharib@gmail.com>
2024-08-30 18:01:01 +0300

    Splash view UI done & Change theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: primaryColor),
       initialRoute: SplashView.id,
       routes: {
         SplashView.id: (context) => const SplashView(),
