d1d0b16d6503ae587efc0b7fb646137efa7a73cc
SaifAllahGharib <saifallahgharib@gmail.com>
2024-09-03 13:28:16 +0300

    Create styles file & Add title to home view

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,4 @@
-class Assets {
+abstract class Assets {
   static const String logo = "assets/images/logo.png";
   static const String imageListItem = "assets/images/test_image.png";
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/books_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/cupertino.dart';
@@ -10,9 +11,15 @@ class HomeViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.only(left: 25, top: 25, right: 10, bottom: 25),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomAppBar(),
           BooksListView(),
+          SizedBox(height: 50),
+          Text(
+            "Best Seller",
+            style: Styles.titleMedium,
+          ),
         ],
       ),
     );
